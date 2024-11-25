97766801d5e38b30efa303a0da5a3c5811c1ffd4
arto1993 <artsargsyan93@gmail.com>
2024-11-25 19:43:40 +0400

    init

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Firebase</title>
+        <link rel="icon" href="/icon.svg" />
+      </Head>
+      <div style={{ overflow: "hidden" }}>
+        <iframe
+          src="https://docs.google.com/presentation/d/e/2PACX-1vRFOpM1Gd9_mSkpsIMVFhbDYpzLWeJiWbbRa2y1DV9OsOY3q5F12ThX9ALq3w2FEEFRu0qQAY1PS0cM/embed?start=false&loop=false&delayms=3000"
+          frameborder="0"
+          width="960"
+          height="569"
+          allowfullscreen="true"
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
